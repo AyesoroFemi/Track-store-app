5d75f669dd3bd4c1eeb1202b692443e4446cab7f
Ayesoro femi <emmafem92@gmail.com>
2020-12-09 02:19:13 +0100

    start change

---
+++ pages/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    
+    <figure class="md:flex bg-gradient-to-r from-red-500 rounded-xl p-8 md:p-0">
+      <img class="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p class="text-lg font-semibold">
+            “Tailwind CSS is the only framework that I've seen scale
+            on large teams. It’s easy to customize, adapts to any design,
+            and the build size is tiny.”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-cyan-600">
+            Sarah Dayan
+          </div>
+          <div class="text-gray-500">
+            Staff Engineer, Algolia
+          </div>
+        </figcaption>
+      </div>
+    </figure>
+
+    <div>
+      <Logo />
+      <h1 class="title">
+        tract-store
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
